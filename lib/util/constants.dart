3ee89dc34abfb9c9a0d607f5410150200da9632b
Santiago Paz Mendieta <spmendieta@outlook.com>
2021-02-06 16:36:49 -0500

    Add basic color palette

---
+++ lib/util/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const PRIMARY_COLOR = LinearGradient(
+  colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+);
+
+const SECONDARY_COLOR = Color(0xFF8B94BC);
+const GREEN_COLOR = Color(0xFF6AC259);
+const RED_COLOR = Color(0xFFE92E30);
+const GRAY_COLOR = Color(0xFFC1C1C1);
+const BLACK_COLOR = Color(0xFF101010);
