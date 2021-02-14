96af9bf380353a309cfdfb864d8c8450d4fb55a9
Santiago Paz Mendieta <spaz@helppeoplecloud.com>
2021-02-13 19:39:54 -0500

    Changed background on Welcome screen

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
 
 class Welcome extends StatelessWidget {
+  final _paddingInsets = EdgeInsets.all(8.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: _createBody(context));
   }
+
+  Widget _createBody(BuildContext context) {
+    return Stack(
+      children: [_createBackground(), _createSafeArea(context)],
+    );
+  }
+
+  Widget _createBackground() {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/icons/bg.jpg"), fit: BoxFit.cover)),
+    );
+  }
+
+  Widget _createSafeArea(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: _paddingInsets,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: _createTextLabels(context),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _createTextLabels(BuildContext context) {
+    return [
+      _createSpacer(),
+      Text(
+        "¿Estás listo para el desafío?",
+        style: Theme.of(context)
+            .textTheme
+            .headline4
+            .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      Text("data"),
+      _createSpacer()
+    ];
+  }
+
+  Widget _createSpacer() => Spacer();
 }
